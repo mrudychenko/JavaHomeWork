62594b7647c72e1f945f14c4bf4683bcae6ed3de
mrudychenko <max.rudychenko@gmail.com>
2021-10-15 21:40:09 +0300

    commit

--- src/com/pb/rudychenko/hw2/Interval.java
+++ src/com/pb/rudychenko/hw2/Interval.java
@@ -4,28 +4,28 @@ import java.util.Scanner;
 
 public class Interval {
     public static void main(String[] args) {
-int d1=14;
-int d2=35;
-int d3=50;
-int d4=100;
 
-int num;
+
+        int l = -1;
+        int d1 = 14;
+        int d2 = 35;
+        int d3 = 50;
+        int d4 = 100;
+
         Scanner input = new Scanner(System.in);
         System.out.print("Введіть число :");
-        num= input.nextInt();
-
-        if (num<=d1){System.out.print("Число потрапляє в інтервал[0-14]") ;
-        }
-else if(num<=d2){
+        int num = input.nextInt();
+        if (num <= l) {
+            System.out.print("Число НЕ потрапляє в інтервал [1-100]");
+        } else if (num <= d1) {
+            System.out.print("Число потрапляє в інтервал[0-14]");
+        } else if (num <= d2) {
             System.out.print("Число потрапляє в інтервал[15-35]");
-        }
-else if(num<=d3){
+        } else if (num <= d3) {
             System.out.print("Число потрапляє в інтервал[36-50]");
-        }
-else if(num>d3 & num<=d4){
+        } else if (num > d3 & num < d4) {
             System.out.print("Число потрапляє в інтервал[51-100]");
-        }
-else {
+        } else if (num > d4) {
             System.out.print("Число НЕ потрапляє в інтервал [1-100]");
         }
     }
